e8b1a0688e4ab0371db709cfeb2bf9d8fcbf8672
TimoOerlemans <t.oerlemans@student.fontys.nl>
2022-04-08 11:18:20 +0200

    Add first table

--- BackendProduct/src/main/java/com/example/backendproduct/Product/Product.java
+++ BackendProduct/src/main/java/com/example/backendproduct/Product/Product.java
@@ -1,4 +1,20 @@
 package com.example.backendproduct.Product;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    public int Id;
+    public String Name;
+    public String Ingredients;
+    public Double Price;
 }
