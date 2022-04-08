039438fb63b1f0a88e82f450b274934ef0796a19
TimoOerlemans <t.oerlemans@student.fontys.nl>
2022-04-08 12:34:39 +0200

    repository

--- BackendProduct/src/main/java/com/example/backendproduct/Product/ProductRepository.java
+++ BackendProduct/src/main/java/com/example/backendproduct/Product/ProductRepository.java
@@ -1,4 +1,6 @@
 package com.example.backendproduct.Product;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
