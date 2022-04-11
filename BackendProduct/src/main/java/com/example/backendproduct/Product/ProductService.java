e1497bf3a3bf7d85dc51a587fceb419e1fa220fd
TimoOerlemans <t.oerlemans@student.fontys.nl>
2022-04-11 09:44:34 +0200

    product service

--- BackendProduct/src/main/java/com/example/backendproduct/Product/ProductService.java
+++ BackendProduct/src/main/java/com/example/backendproduct/Product/ProductService.java
@@ -1,4 +1,14 @@
 package com.example.backendproduct.Product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public ProductService(ProductRepository productRepository){
+        this.repository =productRepository;
+    }
 }
