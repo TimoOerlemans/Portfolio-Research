9149c0f94b0528e74c09932b5fdc779fe1bc5f14
TimoOerlemans <t.oerlemans@student.fontys.nl>
2022-04-13 13:42:10 +0200

    Controller en service

--- src/main/java/com/example/backendproduct/Product/ProductController.java
+++ src/main/java/com/example/backendproduct/Product/ProductController.java
@@ -2,9 +2,9 @@ package com.example.backendproduct.Product;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping
 @RestController
@@ -13,10 +13,28 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @GetMapping()
-    public String getMessage(){
-        return "Welkom";
+    @PostMapping ("/post")
+    public Product addProduct(@RequestBody Product product){
+        return service.saveProduct(product);
     }
 
+    @GetMapping("/products")
+    public List<Product> getProduct(){
+        return service.getProducts();
+    }
 
+    @GetMapping("/product/{id}")
+    public Product findProductById(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product) {
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable int id) {
+        return service.deleteProduct(id);
+    }
 }
--- src/main/java/com/example/backendproduct/Product/ProductService.java
+++ src/main/java/com/example/backendproduct/Product/ProductService.java
@@ -3,6 +3,8 @@ package com.example.backendproduct.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
     @Autowired
@@ -13,4 +15,30 @@ public class ProductService {
     }
 
 
+    public Product saveProduct(Product product){
+        return repository.save(product);
+    }
+
+    public List<Product> getProducts(){
+        return repository.findAll();
+    }
+
+    public Product getProductById(int id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public String deleteProduct(int id){
+        repository.deleteById(id);
+
+        return "You have Deleted" + id;
+    }
+
+    public Product updateProduct(Product product){
+        Product existingProduct = repository.findById(product.getId()).orElse(null);
+        existingProduct.setName(product.getName());
+        existingProduct.setPrice(product.getPrice());
+        existingProduct.setIngredients(product.getIngredients());
+
+        return  repository.save(existingProduct);
+    }
 }
