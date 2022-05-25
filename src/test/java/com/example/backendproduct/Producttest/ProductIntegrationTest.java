2cf3a72465ee549b51cf38b00dfa03c18ab8d2a7
TimoOerlemans <t.oerlemans@student.fontys.nl>
2022-05-25 11:13:00 +0200

    pom file aangepast

--- src/test/java/com/example/backendproduct/Producttest/ProductIntegrationTest.java
+++ src/test/java/com/example/backendproduct/Producttest/ProductIntegrationTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.example.backendproduct.Product.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureMockMvc
 @WebMvcTest(ProductController.class)
 public class ProductIntegrationTest {
     /*
